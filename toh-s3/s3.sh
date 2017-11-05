1ab278016f797666ed5551fb763f543e59698959
Dipesh Patel <dipesh@JiggyMomma.local>
2017-11-05 15:12:57 -0500

    minor update.

--- toh-s3/s3.sh
+++ toh-s3/s3.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
+export BUCKET_PATH = 'my-toh'
+export MY_BUCKET_NAME='s3://$BUCKET_PATH/'
+
 # npm install
 npm install
 
 #  build with ng ;
-ng build --bh /my-toh/ --prod
+ng build --bh /$BUCKET_PATH/ --prod
 
 #  deploy to aws ;
-
-export MY_BUCKET_NAME='s3://my-toh/'
-
 aws s3 cp dist $MY_BUCKET_NAME --recursive --exclude "*" --include "*.txt" --content-type "text/plain" --acl public-read
 aws s3 cp dist $MY_BUCKET_NAME --recursive --exclude "*" --include "*.ico" --content-type "image/x-icon" --acl public-read
 aws s3 cp dist $MY_BUCKET_NAME --recursive --exclude "*" --include "*.html" --content-type "text/html" --acl public-read
@@ -20,4 +20,4 @@ aws s3 cp dist $MY_BUCKET_NAME --recursive --exclude "*" --include "*.js" --cont
 aws s3 website $MY_BUCKET_NAME --index-document index.html
 
 #  url test
-curl https://s3.amazonaws.com/my-toh/index.html
+curl https://s3.amazonaws.com/$BUCKET_PATH/index.html
