622893606b28ed9b609c6a66611c7fdb56644f90
Dipesh Patel <dipesh@JiggyMomma.local>
2018-01-21 20:05:07 -0500

    Add control logic to force update if threshold reached.

--- logger-app/test/rctest.js
+++ logger-app/test/rctest.js
@@ -5,26 +5,34 @@ var arr = [];
 var timer;
 
 var startRange = 100 ;
-var endRange = 200;
+var endRange = 1000;
 
-function longTask(l) {
+function processLongTask() {
+   console.log('arr:  ' + arr.length) ;
+   for (var i = 0; i < 500000; i++) {
+      //  forced delay
+      process.stdout.write('');
+   }
+   arr = [] ;
+   console.log('Done forced delay. arr:  ' + arr.length) ;
+}
+
+function scheduleLongTask(l) {
    arr.push(l) ;
 
-   if (timer) {
-      clearTimeout(timer) ;
-   }
+   if (arr.length === 25) {
+      console.log('Force processing.') ;
+      processLongTask() ;
+   } else {
+      if (timer) {
+         clearTimeout(timer) ;
+      }
 
-   timer = setTimeout(function()
-      {
-         //  var x = arr.splice(0, arr.length) ;
-         console.log('arr:  ' + arr.length) ;
-         for (var i = 0; i < 500000; i++) {
-            //  forced delay
-            process.stdout.write('');
-         }
-         arr = [] ;
-	 console.log('Done forced delay. arr:  ' + arr.length) ;
-      }, Math.floor(Math.random() * ((endRange-startRange)+1) + startRange));
+      timer = setTimeout(function()
+         {
+            processLongTask() ;
+         }, 500) ;
+   }
 }
 
 function getSample(key) {
@@ -35,7 +43,7 @@ function sample1() {
    setTimeout(function()
       {
          console.log('Sample1.  Arr Length:  ' + arr.length) ;
-         longTask(getSample('sample 1:  ')) ;
+         scheduleLongTask(getSample('sample 1:  ')) ;
          sample1() ;
       }, Math.floor(Math.random() * ((endRange-startRange)+1) + startRange));
 }
@@ -44,10 +52,20 @@ function sample2() {
    setTimeout(function()
       {
          console.log('Sample2.  Arr Length:  ' + arr.length) ;
-         longTask(getSample('sample 2:  ')) ;
+         scheduleLongTask(getSample('sample 2:  ')) ;
          sample2() ;
+      }, Math.floor(Math.random() * ((endRange-startRange)+1) + startRange));
+}
+
+function sample3() {
+   setTimeout(function()
+      {
+         console.log('Sample2.  Arr Length:  ' + arr.length) ;
+         scheduleLongTask(getSample('sample 3:  ')) ;
+         sample3() ;
       }, Math.floor(Math.random() * ((endRange-startRange)+1) + startRange));
 }
 
 sample1();
 sample2();
+sample3();
